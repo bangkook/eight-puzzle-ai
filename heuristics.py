b764415406869c08460b8b730ecce52f555f84eb
Mariam Hossam <m.hossam2551@gmail.com>
2023-10-21 23:20:45 +0300

    Implement euclidean heuristic

--- heuristics.py
+++ heuristics.py
@@ -9,6 +9,11 @@ def manhattanHeuristic(state):
 
 
 def euclideanHeuristic(state):
-    # TO BE IMPLEMNTED
-    pass
+    distance = 0
+    for i in range(9):
+        tile = int(state[i])
+        goal_i, goal_j = tile // 3,tile % 3
+        current_i, current_j = i // 3, i % 3
+        distance = distance + ((current_i - goal_i) ** 2 + (current_j - goal_j) ** 2) ** 0.5
+    return distance
 
